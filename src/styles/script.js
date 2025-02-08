a35a6be054346c0c0be17148265b08a40c2a6ecc
enahmedsamy <asamy@blink.global>
2025-02-08 15:23:27 +0300

    design updates

--- src/styles/script.js
+++ src/styles/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const elements = document.querySelectorAll(".fade-in");
-
+  
     elements.forEach((element, index) => {
-        setTimeout(() => {
-            element.style.opacity = "1";
-            element.style.transform = "translateY(0)";
-        }, index * 300); // Delayed animation for staggered effect
+      setTimeout(() => {
+        element.style.opacity = "1";
+        element.style.transform = "translateY(0)";
+      }, index * 300); // Staggered animation delay
     });
-});
+  });
